024987e9ca8e6043e844d4abbbdcf043f873c435
Caleb Mesiti <105940692+CMesiti@users.noreply.github.com>
2026-01-10 21:00:30 -0500

    Backend Structure

--- server/routes/campaignRoutes.py
+++ server/routes/campaignRoutes.py
@@ -1,2 +1,8 @@
 from flask import request, jsonify, Flask
 import requests
+from controllers import campaignController
+
+def add_campaign():
+    data = request.get_json()
+    new_campaign = campaignController.create_campaign()
+    return new_campaign
